0057a79299e60cec11628f28556cb8125675d533
夏宇霆Pro <edward56833517@gmail.com>
2019-04-20 17:09:09 +0800

    jenkins

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
  * @author Edward Xia
  * @date 2019-04-19
 */
+const path = require('path');
+const init = require('./lib/getModules.js');
+const cwd = process.cwd();
+const log = console.log;
+
+let gwsUtils = init.getModules(path.resolve(cwd, './lib'),'.js');
+exports = module.exports = gwsUtils;
