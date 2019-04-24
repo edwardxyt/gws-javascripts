90534684acfaa4d4821811ca9ae332c71bee4036
夏宇霆Pro <edward56833517@gmail.com>
2019-04-24 18:31:19 +0800

    less for edwardxyt

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let version = { version: '0.11.4' };
 import ArraySort from './lib/ArraySort';
 import * as Components from '@edwardxyt/gws-components';
+import rules from "./data/regular";
 
 export {
-    version as default,
-    Components,
-    ArraySort
+    version as default
+    , Components
+    , ArraySort
+    , rules
 }
