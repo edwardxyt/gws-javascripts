15cbc1aefc7c6bd82085ab88a4ef315d91460181
夏宇霆Pro <edward56833517@gmail.com>
2019-08-05 19:34:13 +0800

    Initial commit

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import ArraySort from './lib/ArraySort';
-import * as Components from '@edwardxyt/gws-components';
+import ArraySort from "./lib/arraySort";
+import * as Components from "@edwardxyt/gws-components";
 import rules from "./data/regular";
-import Cache from './lib/cache.js'
-import DOMReadyMethodFactory from './lib/dom-ready-factory.js'
-import NativeBridgeFactory from './lib/native-bridge-factory.js'
+import Cache from "./lib/cache.js";
+import DOMReadyMethodFactory from "./lib/dom-ready-factory.js";
+import NativeBridgeFactory from "./lib/native-bridge-factory.js";
 
-let version = { version: '0.11.4' };
+let version = {version: "0.11.4"};
 let DOMReady = DOMReadyMethodFactory(window, document);
 
 export {
-    version as default
-    , Components
-    , ArraySort
-    , rules
-    , Cache
-    , DOMReady
-    , NativeBridgeFactory
-}
+    version as default,
+    Components,
+    arraySort,
+    rules,
+    Cache,
+    DOMReady,
+    NativeBridgeFactory
+};
