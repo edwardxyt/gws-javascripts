2a8deef5b6ec75ac5d8810fb1100b668fc8e93a4
夏宇霆Pro <edward56833517@gmail.com>
2019-08-07 20:26:45 +0800

    Initial commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import Cache from "./lib/cache.js";
 import DOMReadyMethodFactory from "./lib/dom-ready-factory.js";
 import NativeBridgeFactory from "./lib/native-bridge-factory.js";
 
-let version = require("../package.json").version;
+let version = require("./package.json").version;
 let DOMReady = DOMReadyMethodFactory(window, document);
 
 export {
